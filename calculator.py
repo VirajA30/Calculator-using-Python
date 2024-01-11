c50702e3c1cf78d2fdbfdd601912c821f3645054
VirajA30 <137769122+VirajA30@users.noreply.github.com>
2024-01-12 02:10:28 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1 = int(input('enter a num 1 '))
+num2 = int(input('enter a num 2 '))
+num = input('enter a operator? (-,+,/,*)')
+if (num=='-'):
+   print(num1 - num2)
+elif (num=='+'):
+     print(num1 + num2)
+elif (num=='/'):
+     print(num1 / num2)
+elif (num=='*'):
+     print(num1 * num2)
+else:
+      print('give valid operator....!!!!')
